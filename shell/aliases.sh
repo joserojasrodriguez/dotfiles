49e25373e89b4355cfdaae17c95c46a406aba46c
Jose Antonio <joseantoniorojasrodriguez11@gmail.com>
2020-12-17 19:59:11 +0100

    wip

---
+++ shell/aliases.sh
@@ -0,0 +1,41 @@
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ll="ls -l"
+alias la="ls -la"
+alias cdh="cd ~"
+alias cdc="cd $HOME/Code"
+alias cls="clear"
+
+# Git
+alias gaa="git add -A"
+alias gc="dot git commit"
+alias gca="git add --all && git commit --amend --no-edit"
+alias gco="git checkout"
+alias gd="dot git pretty-diff"
+alias gs="git status -sb"
+alias gf="git fetch --all -p"
+alias gps="git push"
+alias gpsf="git push --force"
+alias gpl="git pull --rebase --autostash"
+alias gb="git branch"
+alias gl="dot git pretty-log"
+alias wip="git add . && git commit -m 'wip'"
+alias nah="git reset --hard && git clean -df"
+
+# Utils
+alias k='kill -9'
+alias i.='(idea $PWD &>/dev/null &)'
+alias c.='(code $PWD &>/dev/null &)'
+alias o.='open .'
+alias phpstorm='open -a "PhpStorm" .'
+
+# Larevel
+alias art="php artisan"
+alias mf="php artisan migrate:fresh"
+alias mfs="php artisan migrate:fresh --seed"
+alias pt="php artisan test"
+alias p="vendor/bin/phpunit"
+alias sail="bash vendor/bin/sail"
